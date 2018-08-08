cfd9a0628d303595c62fdfa1b228ea342dce9b1a
raff <borkowski.rafal22@gmail.com>
2018-08-08 18:08:12 +0200

    Application skeleton added

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loginView.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Drawings Management Program");
+        Image icon = new Image(getClass().getResourceAsStream("/resources/dmpIcon.png"));
+        primaryStage.getIcons().add(icon);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
